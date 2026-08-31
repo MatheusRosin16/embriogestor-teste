d1121a87f9d5cb9f353366cb543c89e401c7ec31
MatheusRosin16 <mrosin16.09@gmail.com>
2026-08-31 10:20:41 -0300

    Update service worker cache version to v13

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
    EmbrioGestor - Service Worker Offline v2
    Funciona no subdiretório do GitHub Pages.
    ============================================================ */
-const CACHE = "embriogestor-pwa-offline-v12";
+const CACHE = "embriogestor-pwa-offline-v13";
 const BASE = self.registration.scope;
 
 function u(path){
